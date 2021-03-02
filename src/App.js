364ae04cc6dc62785fa37aa41b9b18341f966ced
Syed Zayyan Masud <masudzayyan@gmail.com>
2021-03-02 12:18:56 +0000

    done, added relevant css, changed code with suggestion from reddit and woallah

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
-import './App.css';
-import { useState, createContext, useContext } from "react"
+import "./App.css"
+import { useState } from "react"
 import ColorPicker, { useColor } from "react-color-palette";
-
-const ColorContext = createContext();
-
-const ColorContextProvider = (props) => {
-    const [colorCode, setColorCode] = useColor("hex", "#121212");
-    return(
-        <ColorContext.Provider value = {[colorCode, setColorCode]}>
-            {props.children}
-        </ColorContext.Provider>
-    )
-}
-
-function TableData(props) {
-  const [colors, setColors] = useState(false)
-  const [colorCode] = useContext(ColorContext);
-  const backColor = colorCode.hex
-  return <td
-    className="table-cell"
-    style={colors ? ({ backgroundColor: backColor }) : ({ backgroundColor: "white" })}
-    onClick={() => { setColors(prev => !prev); }}
-  >{props.num}</td>
-}
-
-function ColorPalet() {
-  const [colorCode, setColorCode] = useContext(ColorContext);
-  return (
-    <div>
-      <ColorPicker width={456} height={228} color={colorCode} onChange={setColorCode} hideHSB dark />;
-    </div>
-  )
-}
 
 function App() {
   const tableBody = [];
@@ -43,53 +12,63 @@ function App() {
     tableBody[i] = [];
     for (var j = 0; j < columns; j++) {
       if (j === 0) {
-        tableBody[i][j] = i + 1
+        tableBody[i][j] = String.fromCharCode(64 + i + 1)
         console.log(null)
         continue
       }
-      tableBody[i][j] = "";
+      tableBody[i][j] = "white";
     }
   }
-
-  for (var y = 0; y < columns; y++) {
-    if (y === 0) {
-      tableHeader[y] = null
-      continue
-    }
-    tableHeader[y] = String.fromCharCode(63 + y + 1);
+  for(var x =0; x < columns; x ++){
+    tableHeader[x] = x;
   }
 
+  const [colorPalette, SetColorPalette] = useState(tableBody)
+  const [color, setColor] = useColor("hex", "#121212");
+
+  const updateWellColor = (i, j) => {
+    const tempColorPalette = colorPalette;
+    tempColorPalette[i][j] = color.hex;
+    SetColorPalette([...tempColorPalette]);
+  }
+
+
+  if (colorPalette.length < 0) {
+    return <div>Loading</div>
+  }
   return (
-    <div className = "plate-container">
-      <ColorContextProvider>
-      <ColorPalet />
-      <div>
-        <table>
-          <tbody style={{ border: "0.1rem solid black" }}>
-            <tr>
-              {tableHeader.map((x, y) =>
-                <th style={{ fontSize: "2rem" }}>
+    <div>
+      <h1 style = {{textAlign:"center"}}>96 Well Plate Layout Maker</h1>
+      <div className="plate-container">
+        <div>
+          <ColorPicker width={456} height={228} color={color} onChange={setColor} hideHSB dark />
+        </div>
+        <div>
+          <table>
+            <tbody style={{ border: "0.1rem solid black" }}>
+            {tableHeader.map((x, y) =>
+                <th key = {y}>
                   {x}
+                  <br></br>
+                  <div contentEditable = "true" style = {{height:"2rem", width:"2rem"}}></div>
                 </th>
               )}
-            </tr>
-            {
-              tableBody.map((numList, i) => (
-                <tr key={i}>
-                  {
-                    numList.map((num, j) =>
-                      <TableData key={j} num={num} />
-                    )
-                  }
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
+              {
+                colorPalette.map((colorList, i) => (
+                  <tr key={i}>
+                    {
+                      colorList.map((color, j) =>
+                        <td onClick={() => { updateWellColor(i, j) }} key={j} style={{ backgroundColor: color }} className="table-cell"><div className = "table-cell-text">{color}</div></td>
+                      )
+                    }
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
-      </ColorContextProvider>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
